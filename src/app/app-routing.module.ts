ecfb12a86702da0c736ad98e05ec6becb016ba65
cemcak <cem_cakmakci@hotmail.com>
2019-11-17 22:30:37 +0300

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'order-preparing',
     pathMatch: 'full'
   },
   {
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'order-preparing',
+    loadChildren: () => import('./orderPreparing/orderPreparing.module').then(m => m.OrderPreparingModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'OrderPreparing',
+      url: '/order-preparing',
+      icon: 'list'
     }
   ];
 
