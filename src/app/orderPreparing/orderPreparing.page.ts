c0c8ba4fca3d8960577e60d9c6a104f27586a63a
Cem Çakmakçı <cem.cakmakci@ofisim.com>
2019-11-22 21:57:13 +0300

    sss

--- src/app/orderPreparing/orderPreparing.page.ts
+++ src/app/orderPreparing/orderPreparing.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orderp',
@@ -20,17 +22,51 @@ export class OrderPreparing implements OnInit {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  
+  constructor(public actionSheetController: ActionSheetController, public route: Router) {}
+
+  async openModal(abc) {
+    await this.presentActionSheet(abc);
+  }
+
+  async presentActionSheet(abc) {
+    let buttonSet1 = [{
+      text: 'Kedi',
+      icon: 'trash',
+      handler: () => {
+        this.openModal("kedi");
+      }
+    }, {
+      text: 'Köpek',
+      icon: 'share',
+      handler: () => {
+        this.openModal("köpek");
+      }
+    }];
+
+    let buttonSet2 = [{
+      text: 'Dolu',
+      icon: 'trash',
+      handler: () => {
+        this.route.navigateByUrl("/login");
+      }
+    }, {
+      text: 'Dop Dolu',
+      icon: 'share',
+      handler: () => {
+        this.route.navigateByUrl("/login");
+      }
+    }];
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Bağış',
+      cssClass: 'test',
+      buttons: abc == "hayvan" ? buttonSet1 : buttonSet2
+    });
+    await actionSheet.present();
   }
 
   ngOnInit() {
+    
   }
   // add back when alpha.4 is out
   // navigate(item) {
